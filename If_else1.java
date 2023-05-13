367b549339a2e85990658a77e23d3697b163c564
mithunmazumder <130676352+mithunmazumder@users.noreply.github.com>
2023-05-13 19:23:20 +0530

    Read a single Digit Number and write in word

---
+++ If_else1.java
@@ -0,0 +1,42 @@
+package Practice;
+
+        import java.util.Scanner;
+
+public class If_else1 {
+    public static void main(String[] args) {
+        int i;
+        Scanner sc = new Scanner(System.in);
+        i = sc.nextInt();
+        if (i==0) {
+            System.out.println("ZERO");
+        }
+        else if (i==1) {
+            System.out.println("ONE");
+        }
+        else if (i==2) {
+            System.out.println("TWO");
+        }
+        else if (i==3) {
+            System.out.println("THREE");
+        }
+        else if (i==4) {
+            System.out.println("FOUR");
+        }
+        else if (i==5) {
+            System.out.println("FIVE");
+        }
+        else if (i==6) {
+            System.out.println("SIX");
+        }
+        else if (i==7) {
+            System.out.println("SEVEN");
+        }
+        else if (i==8) {
+            System.out.println("EIGHT");
+        }
+        else if (i==9) {
+            System.out.println("NINE");
+        }
+    }
+}
+
